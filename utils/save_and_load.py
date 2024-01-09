e9ed99198d53fd7cbb561ad90d2336e0a85f9905
Parth <parthbidari13@gmail.com>
2024-01-09 17:05:19 +0530

    Utils done

---
+++ utils/save_and_load.py
@@ -0,0 +1,30 @@
+# To get the dataset from the drive direct link and save it locally.
+import requests
+from pathlib import Path
+import os
+
+dir_name = "../Datasets/"
+
+try:
+    os.mkdir(dir_name)
+except FileExistsError:
+    print(f"The directory '{dir_name}' already exists.")
+else:
+    print(f"The directory '{dir_name}' was successfully created.")
+
+def download_file(url, save_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print(f"File downloaded successfully and saved at: {save_path}")
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+
+# url = "https://drive.google.com/uc?export=download&id=1E0r5bjf6MRQT07K5An0jphmRAFU82DEf"
+# save_path = Path("../Datasets/car_info_by_car_dekha.csv.csv")
+        
+url = ""
+save_path = Path("")        
+
+download_file(url, save_path)
